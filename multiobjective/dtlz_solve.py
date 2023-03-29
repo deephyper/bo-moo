c308888a4882bc71d6c577dcf2af72aacf61fbeb
Tyler <tchang@anl.gov>
2023-03-28 19:10:03 -0500

    updated solve to plot bb evals vs. hypervols after running

--- multiobjective/dtlz_solve.py
+++ multiobjective/dtlz_solve.py
@@ -7,10 +7,15 @@ logging.basicConfig(
     force=True,
 )
 
+# Problem parameters
+BB_BUDGET = 200 # 200 eval budget
+NDIMS = 8 # 8 vars
+NOBJS = 3 # 3 objs
+
 # Set DTLZ problem environment variables
 import os
-os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = "5" # 5 vars
-os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = "3" # 2 objs
+os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = str(NDIMS)
+os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = str(NOBJS)
 os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = "2" # DTLZ2 problem
 os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.6" # [x_o, .., x_d]*=0.6
 
@@ -22,6 +27,7 @@ dhb.load("DTLZ")
 # Necessary IF statement otherwise it will enter in a infinite loop
 # when loading the 'run' function from a subprocess
 if __name__ == "__main__":
+    import numpy as np
     from deephyper.problem import HpProblem
     from deephyper.search.hps import CBO
 
@@ -38,13 +44,36 @@ if __name__ == "__main__":
         hpo.run,
         method="process",
         method_kwargs={
-            "num_workers": 2,
+            "num_workers": 4,
         },
     )
 
     # define your search and execute it
-    search = CBO(hpo.problem, evaluator)
+    search = CBO(hpo.problem, evaluator,
+                 moo_scalarization_strategy="Chebyshev")
 
-    # solve with 100 evals
-    results = search.search(max_evals=100)
+    # solve with BB_BUDGET evals
+    results = search.search(max_evals=BB_BUDGET)
     print(results)
+
+    # gather performance stats
+    from deephyper.skopt.moo import pareto_front, hypervolume
+    from ast import literal_eval
+    obj_vals = np.asarray([literal_eval(fi)
+                           for fi in results["objective"].values])
+    hv_vals = []
+    bbf_num = []
+    for i in range(10, BB_BUDGET + 1, 10):
+        hv_vals.append(hypervolume(obj_vals[:i, :], 4.0 * np.ones(NOBJS)) /
+                       (64.0 - np.pi / 3.0))
+        bbf_num.append(i)
+
+    # plot performance over time
+    from matplotlib import pyplot as plt
+    plt.scatter(bbf_num, hv_vals, c='r', s=50, label="")
+    plt.plot(bbf_num, hv_vals, 'k--')
+    plt.xlabel("Number of blackbox function evaluations")
+    plt.ylabel("Percent of total hypervolume dominated")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
