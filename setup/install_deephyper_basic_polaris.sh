bec1825876cc33fe25df4c9cdf73f58278b29e25
Tyler Chang <tchang@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2023-04-13 19:31:06 +0000

    updated deephyper install scripts

--- setup/install_deephyper_basic_polaris.sh
+++ setup/install_deephyper_basic_polaris.sh
@@ -9,32 +9,31 @@
 
 set -xe
 
-module load PrgEnv-gnu/8.3.3
-module load llvm/release-15.0.0
+module load llvm
 module load conda/2022-09-08
 
-conda create -p dhenv --clone base -y
-conda activate dhenv/
+conda create -p dhenv_basic --clone base -y
+conda activate dhenv_basic/
 pip install --upgrade pip
 
 # Install Spack
-git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-. ./spack/share/spack/setup-env.sh
+#git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+#. ./spack/share/spack/setup-env.sh
 
-git clone https://github.com/deephyper/deephyper-spack-packages.git
+#git clone https://github.com/deephyper/deephyper-spack-packages.git
 
 # Install RedisJson With Spack
-spack env create redisjson
-spack env activate redisjson
-spack repo add deephyper-spack-packages
-spack add redisjson
-spack install
+#spack env create redisjson
+#spack env activate redisjson
+#spack repo add deephyper-spack-packages
+#spack add redisjson
+#spack install
 
 # Clone DeepHyper (develop)
 git clone -b develop https://github.com/deephyper/deephyper.git
 
 # Install DeepHyper with Redis
-pip install -e "deephyper/[default,mpi,redis]"
+pip install -e "deephyper/[default,mpi]"
 
 # Install Benchmarks
 git clone https://github.com/deephyper/benchmark.git deephyper-benchmark
@@ -48,16 +47,16 @@ pip install gpustats
 # Copy activation of environment file
 cp ../install/env/polaris.sh activate-dhenv.sh
 echo "" >> activate-dhenv.sh
-echo "conda activate $PWD/dhenv/" >> activate-dhenv.sh
+echo "conda activate $PWD/dhenv_basic/" >> activate-dhenv.sh
 
 # Activate Spack env
 echo "" >> activate-dhenv.sh
 echo ". $PWD/spack/share/spack/setup-env.sh" >> activate-dhenv.sh
-echo "spack env activate redisjson" >> activate-dhenv.sh
+#echo "spack env activate redisjson" >> activate-dhenv.sh
 
 # Redis Configuration
-cp ../install/env/redis.conf redis.confg
-cat $(spack find --path redisjson | grep -o "/.*/redisjson.*")/redis.conf >> redis.conf
+#cp ../install/env/redis.conf redis.confg
+#cat $(spack find --path redisjson | grep -o "/.*/redisjson.*")/redis.conf >> redis.conf
 
 # Install the Combo Benchmark
-python -c 'from deephyper_benchmark import *; install("ECP-Candle/Pilot1/Combo");'
+#python -c "from deephyper_benchmark import *; install("ECP-Candle/Pilot1/Combo");"
