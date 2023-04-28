a6e679cdb4de50dd16b68d0b1ed77df5f2bded55
Tyler <tchang@anl.gov>
2023-04-28 16:43:54 -0500

    updated plotting tools

--- multiobjective/dtlz2/plot_rmse.py
+++ multiobjective/dtlz2/plot_rmse.py
@@ -1,112 +1,49 @@
-from ast import literal_eval
 import csv
-import os
 from matplotlib import pyplot as plt
 import numpy as np
-import pandas
 
-FILENAME = "results.csv"
-PROB_NUM = "2"
-BB_BUDGET = 10000 # 10K eval budget
-NDIMS = 8 # 8 vars
-NOBJS = 3 # 3 objs
-
-# Set DTLZ problem environment variables
-os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = str(NDIMS)
-os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = str(NOBJS)
-os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = PROB_NUM # DTLZ2 problem
-os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.5" # [x_o, .., x_d]*=0.5
-
-import deephyper_benchmark as dhb
-dhb.load("DTLZ")
-from deephyper_benchmark.lib.dtlz.metrics import PerformanceEvaluator
-
-# Create a performance evaluator for this problem
-perf_eval = PerformanceEvaluator()
+# Dirs, names, and colors
+dirs = ["dtlz_mpi_logs-AC", "dtlz_mpi_logs-C", "dtlz_mpi_logs-L",
+        "dtlz_mpi_logs-P", "dtlz_mpi_logs-Q", "pymoo", "parmoo-rbf",
+        "parmoo-axy", "parmoo-tr"]
+labels = ["DeepHyper AugCheb", "DeepHyper Cheb", "DeepHyper Linear",
+          "DeepHyper PBI", "DeepHyper Quad", "NSGA-II (pymoo)",
+          "ParMOO w/ GP", "ParMOO w/ NN", "ParMOO Local"]
+colors = ["g", "r", "b", "c", "m", "y", "brown", "orange", "violet"]
 
 # Gather performance stats
-for PNAME in ["AC", "C", "L", "P", "Q"]:
-    # Read results from CSV file
-    DNAME = "dtlz_mpi_logs-" + PNAME
-    try:
-        results = pandas.read_csv(f"{DNAME}/{FILENAME}")
-        obj_vals = np.asarray([literal_eval(fi) for fi in
-                               results.sort_values("job_id")["objective"].values])
-        # Initialize performance arrays
-        hv_vals = []
-        bbf_num = []
-        for i in range(100, BB_BUDGET, 100):
-            hv_vals.append(perf_eval.rmse(obj_vals[:i, :]))
-            bbf_num.append(i)
-        # Don't forget final budget
-        hv_vals.append(perf_eval.rmse(obj_vals))
-        bbf_num.append(BB_BUDGET)
-        # Add to plot
-        plt.plot(bbf_num, hv_vals, "-o",
-                 label=f"deephyper-{PNAME}")
-    except FileNotFoundError:
-        print(f"skipping deephyper-{PNAME}")
-
-# And add pymoo to plot
-DNAME = "pymoo"
-hv_vals = []
-bbf_num = []
-obj_vals = []
-with open(f"{DNAME}/{FILENAME}", "r") as fp:
-    reader = csv.reader(fp)
-    for i, row in enumerate(reader):
-        for fi in row:
-            obj_vals.append([float(fij) for fij in fi.strip()[1:-1].split()])
-        hv_vals.append(perf_eval.rmse(np.asarray(obj_vals)))
-        bbf_num.append((i+1)*100)
-plt.plot(bbf_num, hv_vals, "-o", label="pymoo/NSGA-II")
-
-# And add parmoo + axy to plot
-DNAME = "parmoo-axy"
-hv_vals = []
-bbf_num = []
-obj_vals = []
-with open(f"{DNAME}/{FILENAME}", "r") as fp:
-    reader = csv.reader(fp)
-    for i, row in enumerate(reader):
-        if i > 0:
-            obj_vals.append([float(fi) for fi in row[-3:]])
-        if i > 100 and (i - 1) % 100 == 0:
-            hv_vals.append(perf_eval.rmse(np.asarray(obj_vals)))
-            bbf_num.append(len(obj_vals))
-plt.plot(bbf_num, hv_vals, "-o", label="parmoo-AXY")
-# And add parmoo + rbf to plot
-DNAME = "parmoo-rbf"
-hv_vals = []
-bbf_num = []
-obj_vals = []
-with open(f"{DNAME}/{FILENAME}", "r") as fp:
-    reader = csv.reader(fp)
-    for i, row in enumerate(reader):
-        if i > 0:
-            obj_vals.append([float(fi) for fi in row[-3:]])
-        if i > 100 and (i - 1) % 100 == 0:
-            hv_vals.append(perf_eval.rmse(np.asarray(obj_vals)))
-            bbf_num.append(len(obj_vals))
-plt.plot(bbf_num, hv_vals, "-o", label="parmoo-RBF")
-# And add parmoo + tr to plot
-DNAME = "parmoo-tr"
-hv_vals = []
-bbf_num = []
-obj_vals = []
-with open(f"{DNAME}/{FILENAME}", "r") as fp:
-    reader = csv.reader(fp)
-    for i, row in enumerate(reader):
-        if i > 0:
-            obj_vals.append([float(fi) for fi in row[-3:]])
-        if i > 100 and (i - 1) % 100 == 0:
-            hv_vals.append(perf_eval.rmse(np.asarray(obj_vals)))
-            bbf_num.append(len(obj_vals))
-plt.plot(bbf_num, hv_vals, "-o", label="parmoo-local")
+for di, DNAME in enumerate(dirs):
+    bbf_num = []
+    hv_vals = []
+    rmse_vals = []
+    for iseed in range(10):
+        FNAME = f"{DNAME}/results_seed{iseed}.csv"
+        try:
+            with open(FNAME, "r") as fp:
+                csv_reader = csv.reader(fp)
+                bbf_num.append([float(x) for x in csv_reader.__next__()])
+                hv_vals.append([float(x) for x in csv_reader.__next__()])
+                rmse_vals.append([float(x) for x in csv_reader.__next__()])
+        except FileNotFoundError:
+            print(f"skipping deephyper-{DNAME}")
+    # Check how many results found
+    n = len(bbf_num)
+    if n > 0:
+        # Plot mean
+        bbf_mean = np.mean(np.array(bbf_num), axis=0)
+        hv_mean = np.mean(np.array(hv_vals), axis=0)
+        rmse_mean = np.mean(np.array(rmse_vals), axis=0)
+        plt.plot(bbf_mean, rmse_mean, "-", color=f"{colors[di]}", label=labels[di])
+        # If more than 1 result, plot std errors
+        if n > 1:
+            hv_stde = np.std(np.array(hv_vals), axis=0) / np.sqrt(n)
+            rmse_stde = np.std(np.array(rmse_vals), axis=0) / np.sqrt(n)
+            plt.fill_between(bbf_num, rmse_mean - rmse_stde, rmse_mean + rmse_stde,
+                             color=f"{colors[di]}", alpha=0.2)
 
 # Add legends and show
 plt.xlabel("Number of blackbox function evaluations")
-plt.ylabel("RMSE over all residuals")
+plt.ylabel("RMSE of Pareto optimal points")
 plt.legend(loc="upper right")
 plt.tight_layout()
 plt.show()
