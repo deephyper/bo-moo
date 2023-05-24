3df4eb5b9013f42de625badcda7fa2d6abb26882
Tyler Chang <tchang@powell.cels.anl.gov>
2023-05-23 20:38:19 -0500

    updated pymoo solver

--- multiobjective/jahs/pymoo_solve.py
+++ multiobjective/jahs/pymoo_solve.py
@@ -1,67 +1,59 @@
 import csv
 import numpy as np
 import os
-from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.core.problem import ElementwiseProblem
+from pymoo.algorithms.moo.nsga2 import NSGA2, RankAndCrowdingSurvival
+from pymoo.core.mixed import MixedVariableGA, MixedVariableMating, MixedVariableSampling, MixedVariableDuplicateElimination
 from pymoo.optimize import minimize
 import sys
 
 from jahs_bench.api import Benchmark
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.variable import Real, Integer, Choice, Binary
 
-# Set the random seed from CL or system clock
-if len(sys.argv) > 1:
-    SEED = int(sys.argv[1])
-else:
-    from datetime import datetime
-    SEED = int(datetime.now().timestamp())
-FILENAME = f"pymoo/results_seed{SEED}.csv"
+class JahsBench(ElementwiseProblem):
 
-# Set default problem parameters
-PROB_NUM = "2"
-BB_BUDGET = 10000 # 10K eval budget
-NDIMS = 8 # 8 vars
-NOBJS = 3 # 3 objs
+    def __init__(self, **kwargs):
+        variables = {
+            "LearningRate": Real(bounds=(1.0e-3, 1.0)),
+            "WeightDecay": Real(bounds=(1.0e-5, 1.0e-3)),
+            "Activation": Choice(options=["ReLU", "Hardswish", "Mish"]),
+            "TrivialAugment": Choice(options=["on", "off"]),
+            "Op1": Integer(bounds=(0, 4)),
+            "Op2": Integer(bounds=(0, 4)),
+            "Op3": Integer(bounds=(0, 4)),
+            "Op4": Integer(bounds=(0, 4)),
+            "Op5": Integer(bounds=(0, 4)),
+            "Op6": Integer(bounds=(0, 4)),
+        }
+        super().__init__(vars=variables, n_obj=2, n_ieq_constr=0, **kwargs)
 
-# Set DTLZ problem environment variables
-os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = str(NDIMS)
-os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = str(NOBJS)
-os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = PROB_NUM # DTLZ2 problem
-os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.5" # [x_o, .., x_d]*=0.5
+        ### JAHS bench settings ###
+        DATASET = "cifar10"
+        MODEL_PATH = "."
+        self.NEPOCHS = 200
+        # Define the benchmark
+        self.benchmark = Benchmark(
+                task=DATASET,
+                save_dir=MODEL_PATH,
+                kind="surrogate",
+                download=True
+        )
+        self.count = 0
 
-# Load DTLZ benchmark suite
-import deephyper_benchmark as dhb
-dhb.load("DTLZ")
-from deephyper_benchmark.lib.dtlz.metrics import PerformanceEvaluator
-
-### JAHS bench settings ###
-DATASET = "cifar10"
-MODEL_PATH = "."
-NEPOCHS = 200
-N_ITERATIONS = 100
-# Define the benchmark
-benchmark = Benchmark(
-        task=DATASET,
-        save_dir=MODEL_PATH,
-        kind="surrogate",
-        download=True
-    )
-
-
-def sim_func(x):
-    """ ParMOO compatible simulation function wrapping jahs-bench.
-
-    Args:
-        x (numpy structured array): Configuration array with same keys
-            as jahs-bench.
-
-    Returns:
-        numpy.ndarray: 1D array of size 2 containing the validation loss
-        (100 - accuracy) and latency.
-
-    """
-
-    sx = np.zeros((x.size[0], 2))
-    for i, xi in enumerate(x):
+    def _evaluate(self, X, out, *args, **kwargs):
+        """ ParMOO compatible simulation function wrapping jahs-bench.
+    
+        Args:
+            x (numpy structured array): Configuration array with same keys
+                as jahs-bench.
+    
+        Returns:
+            numpy.ndarray: 1D array of size 2 containing the validation loss
+            (100 - accuracy) and latency.
+    
+        """
+    
+        sx = []
         # Default config
         config = {
             'Optimizer': 'SGD',
@@ -69,26 +61,42 @@ def sim_func(x):
             'W': 16,
             'Resolution': 1.0,
         }
-        # Update config using x
-        for key in x.dtype.names:
-            config[key] = x[key]
+        # Update config using X
+        for key in X.keys():
+            config[key] = X[key]
         # Special rule for setting "TrivialAugment"
-        if x['TrivialAugment'] == "on":
+        if X['TrivialAugment'] == "on":
             config['TrivialAugment'] = True
         else:
             config['TrivialAugment'] = False
         # Evaluate and return
-        result = benchmark(config, nepochs=NEPOCHS)
-        sx[i, 0] = 100 - result[NEPOCHS]['valid-acc']
-        sx[i, 1] = result[NEPOCHS]['latency']
-    return sx
+        result = self.benchmark(config, nepochs=self.NEPOCHS)
+        sx.append(100 - result[self.NEPOCHS]['valid-acc'])
+        sx.append(result[self.NEPOCHS]['latency'])
+        out["F"] = sx.copy()
+        self.count += 1
+        print(f"finished evaluation {self.count}")
+
+
+# Set the random seed from CL or system clock
+if len(sys.argv) > 1:
+    SEED = int(sys.argv[1])
+else:
+    from datetime import datetime
+    SEED = int(datetime.now().timestamp())
+FILENAME = f"pymoo-cifar/results_seed{SEED}.csv"
+
 
 # Solve DTLZ2 problem w/ NSGA-II (pop size 100) in pymoo
-problem = sim_func
-algorithm = NSGA2(pop_size=50)
-res = minimize(problem,
+algorithm = NSGA2(pop_size=20,
+                  sampling=MixedVariableSampling(),
+                  mating=MixedVariableMating(eliminate_duplicates=MixedVariableDuplicateElimination()),
+                  eliminate_duplicates=MixedVariableDuplicateElimination(),
+                  #survival=RankAndCrowdingSurvival()
+                  )
+res = minimize(JahsBench(),
                algorithm,
-               ("n_gen", 20),
+               ("n_gen", 50),
                save_history=True,
                seed=SEED,
                verbose=False)
