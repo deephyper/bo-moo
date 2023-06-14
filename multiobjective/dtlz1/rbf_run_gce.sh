758db2f51ff48a900c52a53c478bae0d2d08cb7a
Tyler Chang <tchang@homes-01.cels.anl.gov>
2023-06-13 19:29:58 -0500

    added parmoo axy results

--- multiobjective/dtlz1/rbf_run_gce.sh
+++ multiobjective/dtlz1/rbf_run_gce.sh
@@ -6,13 +6,13 @@ export OMP_NUM_THREADS=4
 export OMP_NESTED=false
 export PYTHONPATH=$PYTHONPATH:/home/tchang
 
-python3 parmoo_solve_rbf.py 0
-python3 parmoo_solve_rbf.py 1
-python3 parmoo_solve_rbf.py 2
-python3 parmoo_solve_rbf.py 3
-python3 parmoo_solve_rbf.py 4
-python3 parmoo_solve_rbf.py 5
-python3 parmoo_solve_rbf.py 6
-python3 parmoo_solve_rbf.py 7
+#python3 parmoo_solve_rbf.py 0
+#python3 parmoo_solve_rbf.py 1
+#python3 parmoo_solve_rbf.py 2
+#python3 parmoo_solve_rbf.py 3
+#python3 parmoo_solve_rbf.py 4
+#python3 parmoo_solve_rbf.py 5
+#python3 parmoo_solve_rbf.py 6
+#python3 parmoo_solve_rbf.py 7
 python3 parmoo_solve_rbf.py 8
 python3 parmoo_solve_rbf.py 9
---
+++ multiobjective/jahs/run-parmoo-axy-gce.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export OPENBLAS_NUM_THREADS=1
+export NUM_THREADS=1
+export OMP_NUM_THREADS=4
+export OMP_NESTED=false
+export PYTHONPATH=$PYTHONPATH:/home/tchang/deephyper-benchmark
+
+#python3 parmoo_solve_axy.py 0
+python3 parmoo_solve_axy.py 1
+python3 parmoo_solve_axy.py 2
+python3 parmoo_solve_axy.py 3
+python3 parmoo_solve_axy.py 4
+python3 parmoo_solve_axy.py 5
+python3 parmoo_solve_axy.py 6
+python3 parmoo_solve_axy.py 7
+python3 parmoo_solve_axy.py 8
+python3 parmoo_solve_axy.py 9
