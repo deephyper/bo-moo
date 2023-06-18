932463ee927984a726a493f52f7d56c6b965b8a4
Tyler <tchang@anl.gov>
2023-06-18 18:53:25 -0500

    added dtlz3 dir

---
+++ multiobjective/dtlz3/plot_hv.py
@@ -0,0 +1,53 @@
+import csv
+from matplotlib import pyplot as plt
+import numpy as np
+
+CONF_BOUND = True
+
+# Dirs, names, and colors
+dirs = ["dtlz_mpi_logs-AC", "dtlz_mpi_logs-C", "dtlz_mpi_logs-L",
+        "dtlz_mpi_logs-P", "dtlz_mpi_logs-Q", "pymoo", "parmoo-rbf",
+        "parmoo-tr"]
+labels = ["DeepHyper AugCheb", "DeepHyper Cheb", "DeepHyper Linear",
+          "DeepHyper PBI", "DeepHyper Quad", "NSGA-II (pymoo)",
+          "ParMOO w/ GP", "ParMOO Local"]
+colors = ["g", "r", "b", "c", "m", "y", "orange", "violet"]
+
+# Gather performance stats
+for di, DNAME in enumerate(dirs):
+    bbf_num = []
+    hv_vals = []
+    rmse_vals = []
+    for iseed in range(10):
+        FNAME = f"{DNAME}/results_seed{iseed}.csv"
+        try:
+            with open(FNAME, "r") as fp:
+                csv_reader = csv.reader(fp)
+                bbf_num.append([float(x) for x in csv_reader.__next__()])
+                hv_vals.append([float(x) for x in csv_reader.__next__()])
+                rmse_vals.append([float(x) for x in csv_reader.__next__()])
+        except FileNotFoundError:
+            print(f"skipping {DNAME}")
+    # Check how many results found
+    n = len(bbf_num)
+    if n > 0:
+        # Plot mean
+        bbf_mean = np.mean(np.array(bbf_num), axis=0)
+        hv_mean = np.mean(np.array(hv_vals), axis=0)
+        rmse_mean = np.mean(np.array(rmse_vals), axis=0)
+        plt.plot(bbf_mean, hv_mean, "-", color=f"{colors[di]}", label=labels[di])
+        # If more than 1 result, plot std devs
+        if n > 1 and CONF_BOUND:
+            hv_std = np.std(np.array(hv_vals), axis=0)
+            rmse_std = np.std(np.array(rmse_vals), axis=0)
+            plt.fill_between(bbf_mean, hv_mean - hv_std, hv_mean + hv_std,
+                             color=f"{colors[di]}", alpha=0.2)
+
+# Add legends and show
+plt.xlabel("Number of blackbox function evaluations")
+plt.ylabel("Total hypervolume dominated")
+plt.legend(loc="lower right")
+plt.tight_layout()
+#plt.show()
+plt.savefig("dtlz2_hv.png")
+
---
+++ multiobjective/dtlz3/pymoo_solve.py
@@ -0,0 +1,68 @@
+import csv
+import numpy as np
+import os
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.optimize import minimize
+from pymoo.problems import get_problem
+import sys
+
+# Set the random seed from CL or system clock
+if len(sys.argv) > 1:
+    SEED = int(sys.argv[1])
+else:
+    from datetime import datetime
+    SEED = int(datetime.now().timestamp())
+FILENAME = f"pymoo/results_seed{SEED}.csv"
+
+# Set default problem parameters
+PROB_NUM = "2"
+BB_BUDGET = 10000 # 10K eval budget
+NDIMS = 8 # 8 vars
+NOBJS = 3 # 3 objs
+
+# Set DTLZ problem environment variables
+os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = str(NDIMS)
+os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = str(NOBJS)
+os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = PROB_NUM # DTLZ2 problem
+os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.5" # [x_o, .., x_d]*=0.5
+
+# Load DTLZ benchmark suite
+import deephyper_benchmark as dhb
+dhb.load("DTLZ")
+from deephyper_benchmark.lib.dtlz.metrics import PerformanceEvaluator
+
+# Solve DTLZ2 problem w/ NSGA-II (pop size 80) in pymoo
+problem = get_problem(f"dtlz{PROB_NUM}", n_var=NDIMS, n_obj=NOBJS)
+algorithm = NSGA2(pop_size=80)
+res = minimize(problem,
+               algorithm,
+               ("n_gen", 125),
+               save_history=True,
+               seed=SEED,
+               verbose=False)
+
+# Initialize performance arrays
+rmse_vals = []
+npts_vals = []
+hv_vals = []
+bbf_num = []
+# Create a performance evaluator for this problem and loop over budgets
+perf_eval = PerformanceEvaluator()
+
+# Extract all objective values
+obj_vals = []
+for i, row in enumerate(res.history):
+    for fi in row.result().F:
+        obj_vals.append(fi)
+    hv_vals.append(perf_eval.hypervolume(np.asarray(obj_vals)))
+    rmse_vals.append(perf_eval.rmse(np.asarray(obj_vals)))
+    npts_vals.append(perf_eval.numPts(np.asarray(obj_vals)))
+    bbf_num.append((i+1)*80)
+
+# Dump results to csv file
+with open(FILENAME, "w") as fp:
+    writer = csv.writer(fp)
+    writer.writerow(bbf_num)
+    writer.writerow(hv_vals)
+    writer.writerow(rmse_vals)
