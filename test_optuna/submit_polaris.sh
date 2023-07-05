83ef11b41d9d78aceff8f85438ec93117830f23b
Deathn0t <romainegele@gmail.com>
2023-07-05 17:55:25 +0200

    adding submit_polaris.sh

--- test_optuna/submit_polaris.sh
+++ test_optuna/submit_polaris.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#PBS -l select=2:system=polaris
+#PBS -l select=10:system=polaris
 #PBS -l place=scatter
-#PBS -l walltime=01:00:00
-# #PBS -q prod
-#PBS -q debug
+#PBS -l walltime=03:00:00
+#PBS -q prod
 #PBS -A datascience
 #PBS -l filesystems=grand:home
 
@@ -11,14 +10,15 @@ set -xe
 
 cd ${PBS_O_WORKDIR}
 
-
+# ~~~ EDIT: load your DeepHyper environment here
 source ../../../build/activate-dhenv.sh
 
 #!!! CONFIGURATION - START
-export problem="dhb_combo"
-export search="OPT-TPE"
-export pruning_strategy="SHA"
-export timeout=10800
+# ~~~ EDIT: used to create the name of the experiment folder
+# ~~~ you can use the following variables and pass them to your python script
+export problem="dtlz"
+export search="NSGAII" # TPE is also valid
+export timeout=10200
 export random_state=42
 #!!! CONFIGURATION - END
 
@@ -28,11 +28,11 @@ export NNODES=`wc -l < $PBS_NODEFILE`
 export NTOTRANKS=$(( $NNODES * $NRANKS_PER_NODE ))
 export OMP_NUM_THREADS=$NDEPTH
 
-export log_dir="output/$problem-$search-$pruning_strategy-$NNODES-$timeout-$random_state"
-mkdir -p $log_dir
+export DEEPHYPER_LOG_DIR="output/$problem-$search-$NNODES-$timeout-$random_state"
+mkdir -p $DEEPHYPER_LOG_DIR
 
 ### Setup Postgresql Database - START ###
-export OPTUNA_DB_DIR="$log_dir/optunadb"
+export OPTUNA_DB_DIR="$DEEPHYPER_LOG_DIR/optunadb"
 export OPTUNA_DB_HOST=$HOST
 initdb -D "$OPTUNA_DB_DIR"
 
@@ -43,27 +43,18 @@ echo "host    all             all             .hsn.cm.polaris.alcf.anl.gov      
 sed -i "s/max_connections = 100/max_connections = 2048/" "$OPTUNA_DB_DIR/postgresql.conf"
 
 # start the server in the background and listen to all interfaces
-pg_ctl -D $OPTUNA_DB_DIR -l "$log_dir/db.log" -o "-c listen_addresses='*'" start
+pg_ctl -D $OPTUNA_DB_DIR -l "$DEEPHYPER_LOG_DIR/db.log" -o "-c listen_addresses='*'" start
 
 createdb hpo
 ### Setup Postgresql Database - END ###
 
 sleep 5
 
-export GPUSTAT_LOG_DIR=$PBS_O_WORKDIR/$log_dir
-mpiexec -n ${NNODES} --ppn 1 --depth=1 --cpu-bind depth --envall ../profile_gpu_polaris.sh &
-
 mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} \
     --depth=${NDEPTH} \
     --cpu-bind depth \
     --envall \
-    ../set_affinity_gpu_polaris.sh python -m scalbo.exp --problem $problem \
-    --search $search \
-    --random-state $random_state \
-    --log-dir $log_dir \
-    --pruning-strategy $pruning_strategy \
-    --timeout $timeout \
-    --max-steps 50 
+    ./set_affinity_gpu_polaris.sh python3 dtlz_mpi_solve_optuna.py 0
 
 dropdb hpo
 pg_ctl -D $OPTUNA_DB_DIR -l "$log_dir/db.log" stop
