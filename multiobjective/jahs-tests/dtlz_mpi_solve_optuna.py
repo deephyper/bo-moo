69a0a2eeb32c24356664341f4b98ce06f35a285c
Tyler <tchang@anl.gov>
2023-07-07 18:19:43 -0500

    added optuna test scripts

---
+++ multiobjective/jahs-tests/dtlz_mpi_solve_optuna.py
@@ -0,0 +1,82 @@
+import logging
+import os
+import sys
+import getpass
+
+import mpi4py
+
+mpi4py.rc.initialize = False
+mpi4py.rc.threads = True
+mpi4py.rc.thread_level = "multiple"
+
+from mpi4py import MPI
+from deephyper.search.hps._mpi_doptuna import MPIDistributedOptuna
+
+if not MPI.Is_initialized():
+    MPI.Init_thread()
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+LOG_DIR = os.environ["DEEPHYPER_LOG_DIR"]
+
+username = getpass.getuser()
+host = os.environ["OPTUNA_DB_HOST"]
+storage = f"postgresql://{username}@{host}:5432/hpo"
+print(storage)
+
+# Set the random seed from CL or system clock
+if len(sys.argv) > 1:
+    SEED = int(sys.argv[1])
+else:
+    from datetime import datetime
+
+    SEED = int(datetime.now().timestamp())
+
+# Set default problem parameters
+PROB_NUM = "2"
+BB_BUDGET = 100  # 10K eval budget
+NDIMS = 8  # 8 vars
+NOBJS = 3  # 3 objs
+
+# Set DTLZ problem environment variables
+os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = str(NDIMS)
+os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = str(NOBJS)
+os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = PROB_NUM  # DTLZ2 problem
+os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.5"  # [x_o, .., x_d]*=0.5
+
+# Load DTLZ benchmark suite
+import deephyper_benchmark as dhb
+
+dhb.load("DTLZ")
+from deephyper_benchmark.lib.dtlz import hpo
+
+# Create MPI ranks
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+# Setup info-level logging
+if rank == 0:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - "
+        + "%(message)s",
+        force=True,
+    )
+
+# define the optuna search
+search = MPIDistributedOptuna(
+    hpo.problem,
+    hpo.run,
+    random_state=SEED,
+    log_dir=LOG_DIR,
+    sampler="NSGAII",
+    storage=storage,
+    comm=comm,
+    n_objectives=NOBJS,
+)
+# Solve with BB_BUDGET evals
+results = search.search(max_evals=BB_BUDGET)
+
+MPI.Finalize()
